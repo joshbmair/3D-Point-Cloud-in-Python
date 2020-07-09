55a678eea5b664a7ff72c2ebee907a943e0faeae
Josh Birlingmair <jcbmair@gmail.com>
2020-07-08 22:27:42 -0600

    Fleshed out point debugging

--- PointCloud.py
+++ PointCloud.py
@@ -1,11 +1,13 @@
 debug_point = False
 debug_cloud = False
-EPSILON     = 1e-5
 
 class Point:
+    EPSILON = 1e-5
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        # self.EPSILON = 1e-5
 
     def get_x(self):
         return self.x
@@ -18,7 +20,7 @@ class Point:
         return str
 
     def __eq__(self, p):    # TODO Add feature for if p is not a point
-        if abs(self.x - p.x) < EPSILON and abs(self.y - p.y) < EPSILON:
+        if abs(self.x - p.x) < Point.EPSILON and abs(self.y - p.y) < Point.EPSILON:
             return True
         else: return False
 
@@ -26,7 +28,7 @@ class Point:
         dx = abs(self.x - p.x)
         dy = abs(self.y - p.y)
 
-        return (dy**2 + dy**2) ** 0.5
+        return (dx**2 + dy**2) ** 0.5
 
 class Cloud:
     def __init__(self):
@@ -38,10 +40,24 @@ class Cloud:
 
 debug_point = True
 if debug_point:
-    p0 = Point(0.0, 0.0)
-    p1 = Point(0.0, 0.1)
+    print('Point class debug ON')
+    print(format('EPSILON: {e}', e = Point.EPSILON))
 
-    print(p0 == p1)
+    origin = Point(0.0, 0.0)
+    p1 = Point(0.0, 4.0)
+    p2 = Point(3.0000001, 3.9999999)
+    p3 = Point(3.0, 4.0)
+    p4 = Point(0.0,5.0)
+    p5 = Point(12.0,0.0)
+
+    print("origin: {p}", p = origin)
+    print("p1: {p}", p = p1)
+    print("p2: {p}", p = p2)
+    print("p3: {p}", p = p3)
+    print("p4: {p}", p = p4)
+    print("p5: {p}", p = p5)
+
+else: print('Point class debug OFF')
 
 ####################
 # TEST CLOUD CLASS #
@@ -49,4 +65,4 @@ if debug_point:
 
 # debug_cloud = False
 # if debug_cloud:
-#     print("Hello world")
+#     print("Cloud class debug ON")
